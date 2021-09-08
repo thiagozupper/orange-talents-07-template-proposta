25aee7288457debe05ff9612d83dfe5ed1326a1e
Thiago Ferreira <thiago.silva@zup.com.br>
2021-09-08 14:18:06 -0300

    Login via senha do cartão

---
+++ src/main/java/br/com/zupacademy/thiago/proposta/infra/ResourceServerConfig.java
@@ -0,0 +1,34 @@
+package br.com.zupacademy.thiago.proposta.infra;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
+
+    //Configurações de autorização
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests(authorizeRequests ->
+            {
+                try {
+                    authorizeRequests
+                        .antMatchers(HttpMethod.POST, "/api/propostas/**").hasAuthority("SCOPE_proposta")
+                        .antMatchers(HttpMethod.GET, "/api/propostas/**").hasAuthority("SCOPE_proposta")
+                        .antMatchers(HttpMethod.POST, "/api/propostas/**").hasAuthority("SCOPE_proposta")
+                        .anyRequest().authenticated()
+                        .and().csrf().disable()
+                        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }).oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+    }
+}
